098fbecab1be2fcb62a7b8050db078873a626beb
Darya Khatsuk <raolla12@gmail.com>
2024-04-13 05:16:54 +0300

    Update emails.py

--- manage_app/management/commands/emails.py
+++ manage_app/management/commands/emails.py
@@ -2,13 +2,6 @@ from django.core.mail import EmailMessage, get_connection
 from Client_and_User_Management.settings import EMAIL_ADMIN
 
 
-# def cerate_users_email(send_users=''):
-#     with get_connection() as connection:
-#         EmailMessage(subject='Новые пользователи', body=f"Созданные пользователи для авторизации: {send_users}\n",
-#                      from_email=EMAIL_ADMIN, to=[EMAIL_ADMIN],
-#                      connection=connection).send()
-
-
 def support_email(date='', email='', message='', user='', cookies='', meta=''):
     with get_connection() as connection:
         EmailMessage(subject='Need support for User', body=f"Date: {date}\n"
